917437e1565831732a0ffe7d16e500a419fe355e
Swastik Agarwal <swastik@enlist.io>
2019-09-14 08:13:09 +0530

    Use the right URL

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'cargo'
-set :repo_url, 'git@github.com:swastik/cargo.git'
+set :repo_url, 'git@github.com:kindlyops/cargo.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
