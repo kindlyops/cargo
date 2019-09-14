375d832481642dba98378e1ed2594bbdf586b5bf
Swastik Agarwal <swastik@enlist.io>
2019-09-14 09:12:40 +0530

    Update AWS Config

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,5 @@
-Aws.config.update({
-  :access_key_id => ENV['AWS_ACCESS_KEY'],
-  :secret_access_key => ENV['AWS_SECRET_KEY'],
-  :region => 'us-east-1'
-})
+Aws.config.update(
+  access_key_id: ENV['AWS_ACCESS_KEY'],
+  secret_access_key: ENV['AWS_SECRET_KEY'],
+  region: 'us-east-2'
+)
